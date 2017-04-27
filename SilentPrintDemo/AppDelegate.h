6b06dc9fda8c092c70b84fbcb929703126a507eb
Cuong Trinh <cuong@techmaster.vn>
2017-04-27 16:32:08 +0700

    Initial Commit

---
+++ SilentPrintDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SilentPrintDemo
+//
+//  Created by cuong on 4/27/17.
+//  Copyright © 2017 techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SilentPrintDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SilentPrintDemo
+//
+//  Created by cuong on 4/27/17.
+//  Copyright © 2017 techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
